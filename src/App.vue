16306da1a835aeb261e9ac679ab18536cbe53f32
VashUber <mrjarokvk@gmail.com>
2022-02-10 14:34:08 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app">
+    <v-button>
+      <template #text>Click me</template>
+    </v-button>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from "vue"
+  import vButton from "./components/v-button.vue"
+
+  const counter = ref(0)
+</script>
+
+<style lang="scss">
+  @import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
+
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: "Roboto", sans-serif;
+  }
+
+  .app {
+    font-size: 1rem;
+  }
+</style>
---
+++ src/components/v-button.vue
@@ -0,0 +1,9 @@
+<template>
+  <button>
+    <slot name="text"></slot>
+  </button>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
